973da38578c211490eb5872a095a41e4e95552e3
Damon Blais <albinogeek@gmail.com>
2021-01-18 19:28:24 -0800

    add: test UICellSelect

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -53,3 +53,49 @@ func TestUICells(t *testing.T) {
 		test.AssertImageMatches(t, fmt.Sprintf("cell-center-%d.png", i), w.Canvas().Capture())
 	}
 }
+
+func TestUICellSelect(t *testing.T) {
+	var (
+		a     = test.NewApp()
+		w     = a.NewWindow("Number Place")
+		board = newBoard(3, 3, 3, 3)
+	)
+
+	uiInit(board, w)
+
+	w.Show()
+	a.Run()
+
+	fmt.Printf("%#v", theme.PrimaryColor())
+
+	cells := []*cell{
+		board.cells[0],
+		board.cells[13],
+		board.cells[26],
+		// board.cells[21],
+	}
+
+	// test setSelected
+	for _, c := range cells {
+		c.Select()
+	}
+
+	v := "1"
+	setSelected(board, v)()
+	test.AssertImageMatches(t, "some-set.png", w.Canvas().Capture())
+
+	for _, c := range cells {
+		assert.Equal(t, v, c.Center)
+		assert.Equal(t, false, c.mistake)
+		c.Select()
+	}
+
+	test.AssertImageMatches(t, "some-set-selected.png", w.Canvas().Capture())
+
+	// test clearSelected
+	clearSelected(board)()
+	v = ""
+	for _, c := range cells {
+		assert.Equal(t, v, c.Center)
+	}
+}
