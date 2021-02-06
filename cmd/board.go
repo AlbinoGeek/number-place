a29cb0c7eb925d2bb562475d5c3594693c982482
Damon Blais <albinogeek@gmail.com>
2021-02-05 18:43:18 -0800

    checkColRepeat

--- cmd/board.go
+++ cmd/board.go
@@ -212,27 +212,19 @@ func (b *board) checkColRepeat(duplicates checkerCallback) (err error) {
 	var (
 		cellIDs = make([]int, b.cellsPerCol)
 
-		bx, by, i, j, col, colNum, offset int
+		col, colNum int
 	)
 
-	for bx, colNum = 0, 0; bx < b.boxesWide; bx++ {
-		for col = 0; col < b.boxWidth; col++ {
-			i = 0
-			for by = 0; by < b.boxesTall; by++ {
-				offset = by*b.cellsPerBox*b.boxesWide + bx*b.cellsPerBox + col
+	for col = 0; col < b.cellsPerRow; col++ {
+		for i := 0; i < b.cellsPerCol; i++ {
+			cellIDs[i] = col + i*b.cellsPerCol
+		}
 
-				for j = 0; j < b.boxHeight; j++ {
-					cellIDs[i] = offset + j*b.boxWidth
-					i++
-				}
-			}
-
-			colNum++
-			if items := checkDuplicateIDs(b, cellIDs); len(items) > 0 {
-				err = fmt.Errorf("column %d contains duplicate values", colNum)
-				if duplicates != nil {
-					duplicates(items)
-				}
+		colNum++
+		if items := checkDuplicateIDs(b, cellIDs); len(items) > 0 {
+			err = fmt.Errorf("column %d contains duplicate values", colNum)
+			if duplicates != nil {
+				duplicates(items)
 			}
 		}
 	}
